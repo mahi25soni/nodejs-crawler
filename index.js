81488871e5587f796260f268f9832cfa525febe8
mahi25soni <msdraj02@protonmail.com>
2025-07-22 15:31:39 +0530

    parse text

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ import axios from "axios";
 import { parse } from "node-html-parser";
 
 const getHref = (attibuteString) => {
-  //   const last = attibuteString.lastIndexOf("href");
-
-  //   const startUrl = attibuteString.indexOf('"', last);
-  //   const endUrl = attibuteString.indexOf('"', startUrl + 1);
-
-  //   const url = attibuteString.substring(startUrl + 1, endUrl);
-
   const startingHttp = attibuteString.lastIndexOf("https://");
 
   if (startingHttp === -1) {
@@ -37,16 +30,96 @@ const fetchSeedUrl = async (seedUrl) => {
 
   const parsedData = parse(rawHtmlData);
 
-  const anchorTags = parsedData.querySelectorAll("a");
+  const title = parsedData.querySelector("title")?.text || "";
 
-  const firstOne = anchorTags[1];
-  //   getHref(firstOne.rawAttrs);
+  //   console.log(title);
 
-  anchorTags.map((ele) => {
-    getHref(ele.rawAttrs);
+  //   const metaTags = parsedData.querySelectorAll("meta");
+
+  //   const metadata = {};
+  //   metaTags.forEach((meta) => {
+  //     const name = meta.getAttribute("name") || meta.getAttribute("property");
+  //     const content = meta.getAttribute("content");
+  //     if (name && content) metadata[name] = content;
+  //   });
+
+  //   console.log("Title:", title);
+  //   console.log("Metadata:", metadata);
+  //   const anchorTags = parsedData.querySelectorAll("a");
+
+  //   const hrefs = anchorTags
+  //     .map((link) => link.getAttribute("href"))
+  //     .filter((href) => href && href.startsWith("http"));
+
+  //   console.log("Found links:", hrefs);
+
+  const allH2 = parsedData.querySelectorAll("h2");
+  //   console.log(allH1);
+  allH2.forEach((ele) => {
+    console.log(ele.innerText);
   });
+
+  console.log("------------------------");
+
+  const allH3 = parsedData.querySelectorAll("h3");
+  allH3.forEach((ele) => {
+    console.log(ele.innerText);
+  });
+
+  console.log("------------------------");
+  const allH4 = parsedData.querySelectorAll("h4");
+  allH4.forEach((ele) => {
+    console.log(ele.innerText);
+  });
+  console.log("------------------------");
+  const allH5 = parsedData.querySelectorAll("h5");
+  allH5.forEach((ele) => {
+    console.log(ele.innerText);
+  });
+  console.log("------------------------");
+  const allH6 = parsedData.querySelectorAll("h6");
+  allH6.forEach((ele) => {
+    console.log(ele.innerText);
+  });
+
+  console.log("--------------------------------------------------");
+
+  const blockTags = [
+    "p",
+    "div",
+    "section",
+    "article",
+    "br",
+    "hr",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+  ];
+
+  blockTags.forEach((tag) => {
+    parsedData.querySelectorAll(tag).forEach((el) => {
+      el.insertAdjacentHTML("beforebegin", "\n");
+      el.insertAdjacentHTML("afterend", "\n");
+    });
+  });
+  parsedData
+    .querySelectorAll("script, style, span")
+    .forEach((el) => el.remove());
+  const fullText = parsedData.text
+    .replace(/\r?\n|\r/g, "\n") // normalize line endings
+    .replace(/[ \t]+\n/g, "\n") // remove trailing spaces on lines
+    .replace(/\n{2,}/g, "\n\n") // ensure paragraphs have a line break
+    .trim();
+
+  console.log("Full Text Content:\n", fullText);
 };
 
-const seedUrl = "https://www.linkedin.com/jobs/";
+// Example usage
+
+const seedUrl =
+  "https://www.geeksforgeeks.org/machine-learning/understanding-tf-idf-term-frequency-inverse-document-frequency/";
 
 fetchSeedUrl(seedUrl);
