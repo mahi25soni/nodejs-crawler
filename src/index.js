0b20b37415983acf78e1eb7f9e09d6b8747dd60f
mahi25soni <msdraj02@protonmail.com>
2025-07-29 12:49:47 +0530

    add cors middleware for handling cross-origin requests

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import Express from "express";
 import dotenv from "dotenv";
 import mainRouter from "./routers/mainRouter.js";
 import dbConnect from "./libs/dbConnect.js";
+import cors from "cors";
 
 const app = Express();
 dotenv.config();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 
